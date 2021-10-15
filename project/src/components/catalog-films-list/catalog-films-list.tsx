119ee77097a55d1b5d9c03063977015098a3db4b
Eugene Skrobov <skrobov.ev@gmail.com>
2021-10-15 20:14:33 +0700

    Реализует показ видео при наведении на карточку фильма

--- project/src/components/catalog-films-list/catalog-films-list.tsx
+++ project/src/components/catalog-films-list/catalog-films-list.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import type { Film } from '../../types/types';
 import SmallFilmCard from '../small-film-card/small-film-card';
 
@@ -7,15 +6,9 @@ type CatalogFilmsListProps = {
 }
 
 function CatalogFilmsList({films}: CatalogFilmsListProps): JSX.Element {
-  const [ , setActiveFilm ] = useState<Film | null>(null);
-
-  const changeActiveFilm = (film: Film | null) => {
-    setActiveFilm(film);
-  };
-
   return (
     <div className="catalog__films-list">
-      {films.map((film) => <SmallFilmCard key={film.id} film={film} setActiveFilm={changeActiveFilm} />)}
+      {films.map((film) => <SmallFilmCard key={film.id} film={film} />)}
     </div>
   );
 }
--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,26 +1,71 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import type { Film } from '../../types/types';
 import { AppRoute } from '../../constants';
+import { useEffect, useState, useRef } from 'react';
+
+const HOVER_DELAY = 1000;
+
+const VIDEO_WRAPPER_STYLES = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
 type SmallFilmCardProps = {
   film: Film,
-  setActiveFilm: (film: Film | null) => void,
 }
 
-function SmallFilmCard({film, setActiveFilm}: SmallFilmCardProps): JSX.Element {
-  const handleMouseEnter = () => {
-    setActiveFilm(film);
+function SmallFilmCard({film}: SmallFilmCardProps): JSX.Element {
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const [ isHovered, setHovered ] = useState(false);
+  const [ isDelayedHovered, setDelayedHovered ] = useState(false);
+
+  const history = useHistory();
+
+  const clearTimer = () => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+      timer.current = null;
+    }
   };
 
-  const handleMouseLeave = () => {
-    setActiveFilm(null);
-  };
+  useEffect(() => {
+    clearTimer();
+
+    if (!isHovered) {
+      if (isDelayedHovered) {
+        setDelayedHovered(false);
+      }
+
+      return;
+    }
+
+    timer.current = setTimeout(() => {
+      if (!isHovered) {
+        if (isDelayedHovered) {
+          setDelayedHovered(false);
+        }
+
+        return;
+      }
+
+      setDelayedHovered(true);
+    }, HOVER_DELAY);
+
+    return clearTimer;
+  }, [isHovered]);
 
   return (
-    <article className="small-film-card catalog__films-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <div className="small-film-card__image">
-        <img src={film.posterImage} alt={film.name} width="280" height="175" />
-      </div>
+    <article className="small-film-card catalog__films-card" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} onClick={() => history.push(AppRoute.Film(film.id))}>
+      {
+        isDelayedHovered ?
+          <div style={VIDEO_WRAPPER_STYLES}>
+            <video src={film.previewVideoLink} autoPlay muted poster={film.previewImage} width="280" height="175" style={{objectFit: 'cover'}} />
+          </div> :
+          <div className="small-film-card__image">
+            <img src={film.posterImage} alt={film.name} width="280" height="175" />
+          </div>
+      }
       <h3 className="small-film-card__title">
         <Link to={AppRoute.Film(film.id)} className="small-film-card__link" href="film-page.html">{film.name}</Link>
       </h3>
