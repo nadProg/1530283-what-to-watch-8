ac21617b709254e5f9f6dc66ab602319de207526
Eugene Skrobov <skrobov.ev@gmail.com>
2021-10-15 22:07:50 +0700

    Рефакторит компонент карточки фильма

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -3,7 +3,6 @@ import type { Film } from '../../types/types';
 import { AppRoute } from '../../constants';
 import { useEffect, useState, useRef } from 'react';
 import SmallFilmCardVideo from '../small-film-card-video/small-film-card-video';
-import SmallFilmCardImage from '../small-film-card-image/small-film-card-image';
 
 const BASE_CLASSNAME = 'small-film-card';
 
@@ -57,14 +56,23 @@ function SmallFilmCard({film, className}: SmallFilmCardProps): JSX.Element {
   }, [isHovered]);
 
   return (
-    <article className={fullClassName} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} onClick={() => history.push(AppRoute.Film(film.id))}>
-      {
-        isDelayedHovered ?
-          <SmallFilmCardVideo src={film.previewVideoLink} poster={film.previewImage} /> :
-          <SmallFilmCardImage src={film.previewImage} alt={film.name} />
-      }
+    <article
+      className={fullClassName}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onClick={() => history.push(AppRoute.Film(film.id))}
+    >
+      <SmallFilmCardVideo
+        src={film.previewVideoLink}
+        isPlay={isDelayedHovered}
+        poster={film.previewImage}
+      />
       <h3 className="small-film-card__title">
-        <Link to={AppRoute.Film(film.id)} className="small-film-card__link" href="film-page.html">{film.name}</Link>
+        <Link
+          to={AppRoute.Film(film.id)}
+          className="small-film-card__link"
+        >{film.name}
+        </Link>
       </h3>
     </article>
   );
