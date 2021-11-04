250446d6823033933fd7331aee0d0385991fb0a9
Eugene Skrobov <skrobov.ev@gmail.com>
2021-11-04 21:25:46 +0700

    Покрывает тестами add-review-screen

---
+++ project/src/components/add-review-screen/add-review-screen.test.tsx
@@ -0,0 +1,169 @@
+import { Provider } from 'react-redux';
+import ReactRouter from 'react-router';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { State } from '../../types/types';
+import { AuthorizationStatus, FetchStatus } from '../../constants';
+import { createMockFilm } from '../../mocks/films';
+import AddReviewScreen from './add-review-screen';
+
+const history = createMemoryHistory();
+
+const mockFilm = createMockFilm();
+
+const mockStore = configureMockStore<State>();
+
+describe('Component: AddReviewScreen', () => {
+  it('should render correctly when current is fetched successfully', () => {
+    const successStore = mockStore({
+      films: {
+        currentFilm: {
+          data: mockFilm,
+          status: FetchStatus.Succeeded,
+        },
+      },
+      authorization: {
+        status: AuthorizationStatus.Auth,
+      },
+      comments: {
+        newComment: {
+          status: FetchStatus.Idle,
+        },
+      },
+    });
+
+    successStore.dispatch = jest.fn();
+
+    jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: String(mockFilm.id)});
+
+    render(
+      <Provider store={successStore}>
+        <Router history={history}>
+          <AddReviewScreen />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.queryByText(/This page does not exist/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Go to main page/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Loading Screen/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/WTW/i)).toBeInTheDocument();
+    expect(successStore.dispatch).toHaveBeenCalledTimes(0);
+  });
+
+  it('should show error screen when no valid id is present', () => {
+    const successStore = mockStore({
+      films: {
+        currentFilm: {
+          data: mockFilm,
+          status: FetchStatus.Succeeded,
+        },
+      },
+      authorization: {
+        status: AuthorizationStatus.Auth,
+      },
+      comments: {
+        newComment: {
+          status: FetchStatus.Idle,
+        },
+      },
+    });
+
+    successStore.dispatch = jest.fn();
+
+    jest.spyOn(ReactRouter, 'useParams').mockReturnValue({});
+
+    render(
+      <Provider store={successStore}>
+        <Router history={history}>
+          <AddReviewScreen />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.queryByText(/This page does not exist/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Go to main page/i)).toBeInTheDocument();
+
+    expect(screen.queryByText(/Loading Screen/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/WTW/i)).not.toBeInTheDocument();
+    expect(successStore.dispatch).toHaveBeenCalledTimes(0);
+  });
+
+  it('should loading screen when no current film is present at the time', () => {
+    const initialStore = mockStore({
+      films: {
+        currentFilm: {
+          data: null,
+          status: FetchStatus.Idle,
+        },
+      },
+      authorization: {
+        status: AuthorizationStatus.Auth,
+      },
+      comments: {
+        newComment: {
+          status: FetchStatus.Idle,
+        },
+      },
+    });
+
+    initialStore.dispatch = jest.fn();
+
+    jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: String(mockFilm.id) });
+
+    render(
+      <Provider store={initialStore}>
+        <Router history={history}>
+          <AddReviewScreen />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.queryByText(/Loading Screen/i)).toBeInTheDocument();
+    expect(screen.queryByText(/This page does not exist/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Go to main page/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/WTW/i)).not.toBeInTheDocument();
+
+    expect(initialStore.dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should dispatch load new current film', () => {
+    const initialStore = mockStore({
+      films: {
+        currentFilm: {
+          data: null,
+          status: FetchStatus.Idle,
+        },
+      },
+      authorization: {
+        status: AuthorizationStatus.Auth,
+      },
+      comments: {
+        newComment: {
+          status: FetchStatus.Idle,
+        },
+      },
+    });
+
+    initialStore.dispatch = jest.fn();
+
+    jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: String(mockFilm.id) });
+
+    render(
+      <Provider store={initialStore}>
+        <Router history={history}>
+          <AddReviewScreen />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.queryByText(/Loading Screen/i)).toBeInTheDocument();
+    expect(screen.queryByText(/This page does not exist/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Go to main page/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/WTW/i)).not.toBeInTheDocument();
+
+    expect(initialStore.dispatch).toHaveBeenCalledTimes(1);
+  });
+});
